d2a0a2492b88959437a3cf97b9c5d142cb340684
Sami-20s <samikfouri22@gmail.com>
2021-11-15 20:56:30 +0200

    🧭 🎗 Add Nav Style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,19 @@
 import Head from "next/head";
+import Nav from "../components/Nav/Nav";
+import ContextProvider from "../context/context";
 import "../styles/globals.scss";
+import "../styles/pages/Home/home.scss";
+import "../styles/pages/nav/nav.scss";
 
 function MyApp({ Component, pageProps }) {
     return (
-        <>
+        <ContextProvider>
             <Head>
                 <title>Shortly</title>
             </Head>
+            <Nav />
             <Component {...pageProps} />
-        </>
+        </ContextProvider>
     );
 }
 
