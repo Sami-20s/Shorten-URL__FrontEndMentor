089bdeb3747713e56423f5e739f72dc27b1fa222
Sami-20s <samikfouri22@gmail.com>
2021-11-26 22:08:49 +0200

    🚀 🔧 Update The SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,76 @@ function MyApp({ Component, pageProps }) {
     return (
         <ContextProvider>
             <Head>
-                <title>Shortly</title>
+                <title>Shortly ,More than just shorter links</title>
+                {/* Links For Every Browser Icon  */}
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/img/icon/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/img/icon/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/img/icon/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/img/icon/site.webmanifest" />
+                <link
+                    rel="mask-icon"
+                    href="/img/icon/safari-pinned-tab.svg"
+                    color="#232127"
+                />
+                <meta name="msapplication-TileColor" content="#232127" />
+                <meta name="theme-color" content="#232127" />
+
+                {/* Meta Tags For SEO */}
+                <meta
+                    name="title"
+                    content="Shortly ,More than just shorter links"
+                />
+                <meta
+                    name="description"
+                    content="Free URL shortener to create perfect URLs for your business. Shortly helps you create and share branded links with custom domains at scale."
+                />
+                {/* Facebook */}
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://www.shorten-url.tk/" />
+                <meta
+                    property="og:title"
+                    content="Shortly ,More than just shorter links"
+                />
+                <meta
+                    property="og:description"
+                    content="Free URL shortener to create perfect URLs for your business. Shortly helps you create and share branded links with custom domains at scale."
+                />
+                <meta
+                    property="og:image"
+                    content="/img/illustration-working.svg"
+                />
+                {/* Twitter */}
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta
+                    property="twitter:url"
+                    content="https://www.shorten-url.tk/"
+                />
+                <meta
+                    property="twitter:title"
+                    content="Shortly ,More than just shorter links"
+                />
+                <meta
+                    property="twitter:description"
+                    content="Free URL shortener to create perfect URLs for your business. Shortly helps you create and share branded links with custom domains at scale."
+                />
+                <meta
+                    property="twitter:image"
+                    content="/img/illustration-working.svg"
+                />
             </Head>
             <Nav />
             <Component {...pageProps} />
